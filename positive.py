97a1d9e92875b28c938914f312585d7a133ca7a6
Nalabolu Manikanta <52398617+mani131149@users.noreply.github.com>
2019-07-03 09:09:28 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,9 +1,8 @@
 number=int(input())
-if(number<0)
-print("negative")
-elif(number>0)
-print("positive")
-elif(number==0)
-print("zero")
-else
-print("invalid")
+if number>=0:
+    if number == 0:
+      print("zero")
+    else:
+      print("positive")
+else:
+      print("negative")
